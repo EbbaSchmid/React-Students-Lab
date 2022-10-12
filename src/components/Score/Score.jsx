ebdb5149e18b1ccd6d9e2946316e377fd1354e05
EbbaSchmid <justebba@gmail.com>
2022-10-12 14:18:51 -0500

    css header change

--- src/components/Score/Score.jsx
+++ src/components/Score/Score.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Score = (props) => {
   return ( 
     <div id="studentScore">
-    <h4>Score Information</h4>
+    <h3>Score Information</h3>
     <h4>Score Date: {props.score.date}</h4>
     <h4>Score: {props.score.score}</h4>
     </div>

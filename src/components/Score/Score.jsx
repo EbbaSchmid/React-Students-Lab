121d2b9465aca33416de4a3d64e11d7e31fe8b44
EbbaSchmid <justebba@gmail.com>
2022-10-12 08:19:40 -0500

    score.jsx

---
+++ src/components/Score/Score.jsx
@@ -0,0 +1,11 @@
+const Score = (props) => {
+  return ( 
+    <div>
+    <h4>Score Component</h4>
+    <h4>{props.score.date}</h4>
+    <h4>{props.score.score}</h4>
+    </div>
+  );
+}
+
+export default Score;
